88221ff8e93523d95ac66c5f08c65ae6df50ca76
mishu <mishu@xtelecom.ro>
2019-02-20 16:12:18 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Dropdown
+
+[![License](https://poser.pugx.org/laravel-enso/dropdown/license)](https://packagist.org/packages/laravel-enso/dropdown)
+[![Latest Stable Version](https://poser.pugx.org/laravel-enso/dropdown/version)](https://packagist.org/packages/laravel-enso/dropdown)
+
+Vue Dropdown component
+
+### Features
+
+- soon
+
+### Configuration & Usage
+
+- soon
+
+### Contributions
+
+are welcome. Pull requests are great, but issues are good too.
+
+### License
+
+This package is released under the MIT license.
---
+++ src/bulma/Dropdown.vue
@@ -0,0 +1,84 @@
+<template>
+    <div class="dropdown is-active"
+        v-click-outside="hide">
+        <div class="dropdown-trigger"
+            @click="hidden = !hidden"
+            v-click-outside="attemptHide">
+            <button class="button">
+                <slot name="label"/>
+                <dropdown-indicator v-model="hidden"/>
+            </button>
+        </div>
+        <fade>
+            <div class="animated dropdown-menu menu-list"
+                v-if="!hidden"
+                :style="widthStyle">
+                <div class="dropdown-content has-text-centered"
+                    :style="[widthStyle, heightStyle]">
+                    <slot/>
+                </div>
+            </div>
+        </fade>
+    </div>
+</template>
+
+<script>
+
+import vClickOutside from 'v-click-outside';
+import DropdownIndicator from '@enso-ui/dropdown-indicator';
+import { Fade } from '@enso-ui/transitions';
+
+export default {
+    name: 'Dropdown',
+
+    directives: {
+        clickOutside: vClickOutside.directive,
+    },
+
+    components: { DropdownIndicator, Fade },
+
+    props: {
+        height: {
+            type: Number,
+            default: 16,
+        },
+        manual: {
+            type: Boolean,
+            default: false,
+        },
+        width: {
+            type: Number,
+            default: 4.5,
+        },
+    },
+
+    data: () => ({
+        hidden: true,
+    }),
+
+    computed: {
+        heightStyle() {
+            return {
+                'max-height': `${this.height}em`,
+            };
+        },
+        widthStyle() {
+            return {
+                'min-width': `${this.width}em`,
+            };
+        },
+    },
+
+    methods: {
+        hide() {
+            this.hidden = true;
+        },
+        attemptHide() {
+            if (!this.manual) {
+                this.hide();
+            }
+        },
+    },
+};
+
+</script>
